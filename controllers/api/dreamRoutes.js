3aca639aabfe9b12f88d985bc00fdf53965b5077
Zo Beki <uobeki.40@gmail.com>
2021-03-31 17:12:58 -0400

    api controllers

--- controllers/api/dreamRoutes.js
+++ controllers/api/dreamRoutes.js
@@ -4,31 +4,36 @@ const withAuth = require('../../utils/auth')
 
 router.post('/', withAuth, async (req, res) => {
     try {
-        const newDream = await Dream.create({
+        const dreamData = await Dream.create({
             ...req.body,
             user_id: req.session.user_id
         });
 
-        res.status(200).json(newDream);
+        res.status(200).json(dreamData);
     } catch (err) {
         res.status(400).json(err)
     };
 });
 
 router.put('/:id', withAuth, (req, res) => {
-    Dream.update(
-        {
-            subject: req.body.subject,
-            body: req.body.body
-        },
-        {
-            where: {
-                id: req.params.id,
-                user_id: req.session.user_id
+    try {
+        Dream.update(
+            {
+                subject: req.body.subject,
+                body: req.body.body
+            },
+            {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id
+                }
             }
-        }
-    )
-})
+        )
+        res.status(200).json({ message: 'Success!' })
+    } catch (err) {
+        res.status(500).json(err);
+    }  
+});
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const dreamRoutes = require('./dreamRoutes')
+const dreamRoutes = require('./dreamRoutes');
+const { User } = require("../../models");
+const { Dream } = require("../../models");
 
 router.use("/users", userRoutes);
 router.use("/dreams", dreamRoutes);
 
 module.exports = router;
+
+
