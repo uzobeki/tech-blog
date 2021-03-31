68328f3063a7e1c9dd81a4bd9d41d0bf53ae1a43
Zo Beki <uobeki.40@gmail.com>
2021-03-31 17:21:04 -0400

    new js files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,8 +5,14 @@ const zodiac = require('zodiac-signs')();
 
 router.get("/", async (req, res) => {
   try {
+    const dreamData = await Dream.findAll({
+      include: [{ model: User }]
+    });
+    const dreams = dreamData.map((dream) => dream.get({ plain: true }));
+    console.log(dreams)
     res.render("homepage", {
       logged_in: req.session.logged_in,
+      dreams
     });
   } catch (err) {
     res.status(500).json(err);
@@ -37,7 +43,7 @@ router.get("/profile", withAuth, async (req, res) => {
     const user = userData.get({ plain: true });
     const sign = zodiac.getSignByDate({ day: userData.day, month: userData.month })
     const dreams = dreamData.map((dream) => dream.get({ plain: true }));
-    
+
     res.render("profile", {
       ...user,
       logged_in: true,
@@ -49,6 +55,15 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+router.get('/new-dreams', withAuth, (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  res.render('new-dreams');
+})
+
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/profile");
@@ -56,6 +71,23 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+router.get('/:id', withAuth, async (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login')
+  }
+
+  try {
+      const dreamData = await Dream.findByPk(req.params.id)
+      const dream = dreamData.get({ plain: true });
+
+      res.render('edit-dreams', {
+          ...dream
+      })
+  } catch (err) {
+      res.status(500).json(err);
+  }
 });
 
 module.exports = router;
